223d6e312272ec779013742a6b8d2490027c6d66
Nazir <C09616@cerner.net>
2024-02-05 17:51:35 +0400

    Search button

--- app/topic/addTopic/page.jsx
+++ app/topic/addTopic/page.jsx
@@ -22,7 +22,7 @@ export default function AddTopic() {
 
         try {
 
-            const res = await fetch("http://localhost:3000/api/topics", {
+            const res = await fetch("https://notes-lilac-sigma.vercel.app/api/topics", {
                 method: "POST",
                 headers: {
                     "Content-type": "application/json",
